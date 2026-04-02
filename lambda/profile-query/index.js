a018a457a77dc562554698ea9d0718061525a45b
nikolai <144839823+unitnikolai@users.noreply.github.com>
2026-04-02 17:37:29 -0400

    Add files via upload

--- lambda/profile-query/index.js
+++ lambda/profile-query/index.js
@@ -1,8 +1,8 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 
 export const handler = async (req, res) => {
     const userId = req.user.sub;
-    const client = new Client({
+    const pool = new Pool({
       host: creds.host, 
       user: creds.username,
       password: creds.password,
@@ -11,11 +11,16 @@ export const handler = async (req, res) => {
       ssl: true,
       connectionTimeoutMillis: 5000
     });
-
-    await client.connect();
-
-    await client.query(
-        "SELECT email, given_name, family_name, organization_id FROM users WHERE id = $1",
-        [id]
-    )
+    try {
+        const { user_info } = await pool.query(
+            "SELECT email, given_name, family_name, organization_id FROM users WHERE id = $1",
+            [userId]
+        );
+        if (!user_info[0]){
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.json(user_info[0]);
+    } finally {
+        await pool.release()
+    }
 }
